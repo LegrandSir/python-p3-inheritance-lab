a88f271a3e493208d2ccacd9cda122b46956b2bf
Paul Wafula <paulwafula391@gmail.com>
2025-07-19 13:55:45 +0300

    complete

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,22 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, info):
+        self.knowledge.append(info)
+
+    #def knowledge(self):
+    
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
-
+# teacher.py
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Variables store values",
+            "Loops help automate repetitive tasks",
+            "Functions encapsulate logic",
+            "Classes define object blueprints"
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
